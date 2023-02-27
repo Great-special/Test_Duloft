1a70be72eadfbfce29debc0e10fe031e94d48d3e
Great Special <sirgreatspecial@gmail.com>
2023-02-27 10:46:53 +0100

    DAY 5 First-Develop

--- Agent_pro/settings.py
+++ Agent_pro/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://find-house.up.railway.app']
 
 # Application definition
 
